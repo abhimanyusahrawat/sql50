240e6e37fc0fcae032840e1cbbb53f2fb571ad08
abhimanyuSahrawat <abhimanyumaxfort@gmail.com>
2024-02-07 23:38:43 +0530

    1148_2_solution

--- 1148_Arictle_views.sql
+++ 1148_Arictle_views.sql
@@ -1,3 +1,3 @@
 
---  as we  need return heading as id hence using "as" ALIAS keyword in sql and next we just wanted to equate autID and viewId and return the answer in ascending order hence used order by
+--  as we  need to return heading as id hence using "as" ALIAS keyword in sql and next we just wanted to equate autID and viewId and return the answer in ascending order hence used order by
 SELECT DISTINCT author_id AS id FROM Views where author_id = viewer_id ORDER BY id;
